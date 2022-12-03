1347aed228bdd54ca81d03e2400e7de87f993f00
Olga Lemeshevskaya <birtochka@gmail.com>
2022-12-03 01:21:48 -0800

    fix: change validate fields in form 'Add Event'

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,38 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import moment from "moment";
 
 export default function AddEvent({ event }) {
-  const startDay = '09:00';
-  const endDay = '21:00';
-  function validatestring(value) {
-    let error;
-    if (!value) {
-      error = "This field is required";
-    } else if (typeof value !== "string") {
-      error = "This field must be text and cannot contain only numbers";
-    }
-    return error;
-  }
+  const startDay = "09:00";
+  const endDay = "21:00";
+  // const [startTime, setStartTime] = useState("");
+  // const [endTime, setEndTime] = useState("");
 
-  function validatetime(value) {
-    let error;
-    if (value.toString() < startDay || value.toString() > endDay) {
-      error = 'The time must be between 9 am and 9 pm.'
-      console.log(value.toString());
-      console.log(startDay);
-      console.log(value.toString() < startDay);
-    }
-    return error;
-  } 
-  function validateperiod(value) {
-    let error;
-    if(formik.value.starttime > value.endtime) {
-      error = "TTime";
-      console.log(error)
-    }
-    return error
-
-  }
+  const EventSchema = Yup.object().shape({
+    eventname: Yup.string()
+      .required("This field is required")
+      .test(
+        "not-only-number",
+        "This field must be text and cannot contain only numbers",
+        (value) => (value ? !value.match(/^\d+$/) : false)
+      ),
+    location: Yup.string()
+      .required("This field is required")
+      .test(
+        "not-only-number",
+        "This field must be text and cannot contain only numbers",
+        (value) => (value ? !value.match(/^\d+$/) : false)
+      ),
+    startTime: Yup.string()
+      .required("This field is required")
+      .test(
+        "period",
+        "The time must be between 9 am and 9 pm.",
+        (value) =>
+          moment(value, "HH:mm").isSameOrAfter(moment(startDay, "HH:mm")) &&
+          moment(value, "HH:mm").isSameOrBefore(moment(endDay, "HH:mm"))
+      ),
+    endTime: Yup.string()
+      .required("This field is required")
+      .test(
+        "period",
+        "The time must be between 9 am and 9 pm.",
+        (value) =>
+          moment(value, "HH:mm").isSameOrAfter(moment(startDay, "HH:mm")) &&
+          moment(value, "HH:mm").isSameOrBefore(moment(endDay, "HH:mm"))
+      )
+      .when("startTime", (startTime, schema) =>
+        schema.test("is-greater", "end time should be greater", (value) =>
+          moment(value, "HH:mm").isSameOrAfter(moment(startTime, "HH:mm"))
+        )
+      ),
+  });
 
   return (
     <>
@@ -41,63 +56,45 @@ export default function AddEvent({ event }) {
         initialValues={{
           eventname: "",
           location: "",
-          starttime: "",
-          endtime: "",
+          startTime: "",
+          endTime: "",
         }}
+        validationSchema={EventSchema}
         onSubmit={(values) => {
-          let error;
           console.log(values);
-          if(values.starttime > values.endtime) {
-             error = "TTime";
-             console.log(error)
-             return error
-          } else {
-            console.log(values);
-          }
-         
         }}
       >
         {({ errors, touched }) => (
           <Form>
             <label>
               Name event
-              <Field name="eventname" validate={validatestring} />
+              <Field name="eventname" />
               {errors.eventname && touched.eventname && (
                 <div>{errors.eventname}</div>
               )}
             </label>
             <label>
               Location
-              <Field name="location" validate={validatestring} />
+              <Field name="location" />
               {errors.location && touched.location && (
                 <div>{errors.location}</div>
               )}
             </label>
             Start time
             <label>
-              <Field
-                type="time"
-                name="starttime"
-                validate={validatetime}
-              />
-              {errors.starttime && touched.starttime && (
-                <div>{errors.starttime}</div>
+              <Field type="time" name="startTime" />
+              {errors.startTime && touched.startTime && (
+                <div>{errors.startTime}</div>
               )}
             </label>
             <label>
               End time
-              <Field type="time" name="endtime" validate= {validateperiod}/>
-              {errors.endtime && touched.endtime && (
-                <div>{errors.endtime}</div>
-              )}
+              <Field type="time" name="endTime" />
+              {errors.endTime && touched.endTime && <div>{errors.endTime}</div>}
             </label>
-            <button type="submit" text="Add event" name = 'addevent' disabled={!errors} >
-            
+            <button type="submit" text="Add event" disabled={!errors}>
               Submit
             </button>
-            {errors.addevent && touched.addevent && (
-                <div>{errors.addevent}</div>
-              )}
           </Form>
         )}
       </Formik>
